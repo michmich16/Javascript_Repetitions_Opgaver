7d6f713cd62e434e931a8c87478057c8713ff905
Michael P <102651473+michmich16@users.noreply.github.com>
2024-06-11 11:32:07 +0200

    ppp

--- js/lektion_1.js
+++ js/lektion_1.js
@@ -12,8 +12,45 @@ function fullName(firstName, lastName) {
   newH3.innerText = greeting;
   L1_1.appendChild(newH3);
 }
+fullName()
 
 // Opgave 2
-
 // Function Calls
-fullName("Ukendt", "Person");
+function toggleHide() {
+  let element = document.getElementById("L1_2");
+  if (element.classList.toggle("hidden")) {
+    console.log("element is hidden");
+  }};
+
+
+
+// opgave 3
+//3. Lav en funktion der farver et element når man klikker på en knap og viser en besked til brugeren om at farven er ændret. Når man trykker på knappen igen skal farven fjernes.
+function farverElement() {
+  let element = document.getElementById("L1_3");
+  element.style.background ='#fe0000';
+  element.innerHTML += "color is changed";
+  console.log("color has changed");
+}
+
+//opgave 4
+// Lav en funktion der tæller hvor mange gange brugeren har klikket på en knap og vis antallet af klik, som det opdateres, i DOM´en.
+function clickCounter() {
+let count = 0;
+let btn = document.getElementById("clickButton");
+let element = document.getElementById("L1_4");
+ 
+btn.addEventListener("click", () => {
+    count++;
+    element.innerHTML += `button has been click ${count} times`;
+
+
+})};
+clickCounter()
+
+//opgave 5
+// 5. Lav en funktion der kan tage imod et ord som argument. Funktionen skal kunne tælle hvor langt ordet er, vise ordet i DOM´en og fortælle brugeren hvor langt ordet er.
+function countWord() {
+  let count = 0;
+  
+}
