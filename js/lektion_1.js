b478fc39c9a7e18ad1e5fe61fc7b44a54de447b3
Michael P <102651473+michmich16@users.noreply.github.com>
2024-06-14 11:47:38 +0200

    l1_3

--- js/lektion_1.js
+++ js/lektion_1.js
@@ -16,41 +16,62 @@ fullName()
 
 // Opgave 2
 // Function Calls
-function toggleHide() {
-  let element = document.getElementById("L1_2");
-  if (element.classList.toggle("hidden")) {
-    console.log("element is hidden");
-  }};
+function hideThis() {
+  let hideButton = document.createElement('button');
+  hideButton.innerText = 'Hide';
+
+  let hideText = document.createElement('h3');
+  hideText.innerText = 'Please hide this message!';
+
+  hideButton.addEventListener('click', () => {
+    hideText.classList.toggle('hidden');
+    if (hideButton.innerText === 'Hide') {
+      hideButton.innerText = 'Show';
+      console.log('message is hidden');
+    } else {
+      hideButton.innerText = 'Hide';
+      console.log('message is shown');
+    }
+  });
+  L1_2.appendChild(hideButton)
+  L1_2.appendChild(hideText)
+};
 
 
+hideThis()
 
 // opgave 3
 //3. Lav en funktion der farver et element når man klikker på en knap og viser en besked til brugeren om at farven er ændret. Når man trykker på knappen igen skal farven fjernes.
-function farverElement() {
-  let element = document.getElementById("L1_3");
-  element.style.background ='#fe0000';
-  element.innerHTML += "color is changed";
-  console.log("color has changed");
+function colorThis() {
+  let colorButton = document.createElement('button');
+  colorButton.innerText = 'Color it red!';
+
+  let colorMessage = document.createElement('p');
+  colorMessage.innerText = '';
+
+  colorButton.addEventListener('click', () =>{
+  L1_3.style.background = 'red';
+  colorMessage.innerText = 'Color has been changed!';
+  
+  if(colorButton.innerText === 'Color it red!'){
+    colorButton.innerText = 'Remove red!';
+    L1_3.style.background = 'none';
+    colorMessage.innerText = 'Color has been removed!';
+  } else{
+    colorButton.innerText = 'Color it red!';
+    L1_3.style.background = 'red';
+    colorMessage.innerText = 'Color has been changed!';
+  }
+  })
+  L1_3.appendChild(colorButton);
+  L1_3.appendChild(colorMessage);
 }
+
+colorThis();
 
 //opgave 4
-// Lav en funktion der tæller hvor mange gange brugeren har klikket på en knap og vis antallet af klik, som det opdateres, i DOM´en.
-function clickCounter() {
-let count = 0;
-let btn = document.getElementById("clickButton");
-let element = document.getElementById("L1_4");
- 
-btn.addEventListener("click", () => {
-    count++;
-    element.innerHTML += `button has been click ${count} times`;
+//4. Lav en funktion der tæller hvor mange gange brugeren har klikket på en knap og vis antallet af klik, som det opdateres, i DOM´en.
 
-
-})};
-clickCounter()
 
 //opgave 5
-// 5. Lav en funktion der kan tage imod et ord som argument. Funktionen skal kunne tælle hvor langt ordet er, vise ordet i DOM´en og fortælle brugeren hvor langt ordet er.
-function countWord() {
-  let count = 0;
-  
-}
+//5. Lav en funktion der kan tage imod et ord som argument. Funktionen skal kunne tælle hvor langt ordet er, vise ordet i DOM´en og fortælle brugeren hvor langt ordet er.
