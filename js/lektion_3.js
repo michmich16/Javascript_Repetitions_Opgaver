c99a01137d6d00cc6f4f364413f39f25a2fa05b1
Michael P <102651473+michmich16@users.noreply.github.com>
2024-06-14 10:28:58 +0200

    r

--- js/lektion_3.js
+++ js/lektion_3.js
@@ -3,3 +3,16 @@ let L3_2 = document.querySelector("#L3_2");
 let L3_3 = document.querySelector("#L3_3");
 let L3_4 = document.querySelector("#L3_4");
 let L3_5 = document.querySelector("#L3_5");
+
+
+const classArray = ['Makka', 'Elena', 'Mathias', 'Peter', 'Christian'];
+
+function displayClassNames() {
+    for (let i = 0; i < classArray.length; i++) {
+        let newElement = document.createElement('p');
+        newElement.innerText = classArray[i];
+        L3_1.appendChild.newElement;
+    }
+}
+
+displayClassNames()
